086d0da709a6315199357c2c165b6e10fedba0d8
Taarini1 <98458225+Taarini1@users.noreply.github.com>
2023-08-31 17:41:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-canvas = document.getElementById("myCanvas")
-ctx=canvas.getContext("2d")
-
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 img_width = 300;
 img_height = 100;
 
@@ -10,7 +9,7 @@ img_x = 100;
 img_y = 100;
 
 function add() {
-	img_imgTag = new Image();  img_imgTag.src = img_imgTag;
+	img_imgTag = new Image(); //defining a variable with a new image
 	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
 	img_imgTag.src = img_image;   // load image
 }
@@ -20,43 +19,42 @@ function uploadimg() {
 	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-window.addEventListener("keydown",my_keydown)
+window.addEventListener("keydown" , my_keydown);
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
-		
-			aplhabetkey();
-		document.getElementById("d1").innerHTML="You Pressed Alphabet key";
-		console.log("alphabet key");
-		else{
+		alphabetkey();
+	document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+	console.log("alphabet key");
+	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
 	}
 }
 
-function aplhabetkey()
+function alphabetkey()
 {
-	img_image="alpkey.png";
-	add()
+	img_image="Alpkey.png"
+	add();
 
 }
 function numberkey()
 {
-	img_image="numkey.png";
-	add()
+	img_image="numkey.png"
+	add();	
 }
 function arrowkey()
 {
-	img_image="arrkey.png";
-	add()
+	img_image="Arrkey.png"
+	add();
 }
 function specialkey()
 {
-	img_image="spkey.png";
-	add()
+	img_image="spkey.png"
+	add();
 }
 function otherkey()
 {
